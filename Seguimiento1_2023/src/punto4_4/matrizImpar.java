14795be11c6c25bff47d2f95621b1f773abfc08f
AlissonCamposMar <100102366+AlissonCamposMar@users.noreply.github.com>
2023-04-08 11:01:52 -0500

    documentación de numSmith iterativo y recursivo

--- Seguimiento1_2023/src/punto4_4/matrizImpar.java
+++ Seguimiento1_2023/src/punto4_4/matrizImpar.java
@@ -1,4 +1,31 @@
 package punto4_4;
 
 public class matrizImpar {
+    public static void main(String[] args) {
+        int n = 5; // Tamaño de la matriz
+        int[][] matriz = new int[n][n]; // Declaración de la matriz
+
+        // Llenado de la matriz
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                if (i < n / 2) {
+                    if (j >= i && j < n - i) {
+                        matriz[j][i] = 1;
+                    }
+                } else {
+                    if (j >= n - i - 1 && j <= i) {
+                        matriz[j][i] = 1;
+                    }
+                }
+            }
+        }
+
+        // Impresión de la matriz
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
