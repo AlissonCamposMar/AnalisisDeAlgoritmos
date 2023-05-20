d4407694053523c40d0c93a6ce2ec2a38578d19d
AlissonCamposMar <alisson.camposm@uqvirtual.edu.co>
2023-05-20 12:01:44 -0500

    indu recursivo estatico corregido

--- 2022/src/parcialParte1/HinduRecursivaEstatico.java
+++ 2022/src/parcialParte1/HinduRecursivaEstatico.java
@@ -2,76 +2,69 @@ package parcialParte1;
 
 public class HinduRecursivaEstatico {
 
-    public static void main(String[] args){
-        int [] arr1 = {9,9,9,9,9,9,9,9,9};
-        int [] arr2 = {9,9,9,9,9,9,9,9,9};
+    public static void main(String[] args) {
+        int[] arr1 = {9,9,9,9,9,9,9,9,9};
+        int[] arr2 = {9,9,9,9,9,9,9,9,9};
 
         System.out.println("Arreglo multiplicando");
-        for(int l: arr1)
-            System.out.print(l + " ");
+        imprimirArreglo(arr1);
 
         System.out.println("\nArreglo multiplicador");
-        for (int h : arr2)
-            System.out.print(h + " ");
+        imprimirArreglo(arr2);
         System.out.println();
 
-        multiplicarIndu(arr1, arr2);
+        int[] resultado = multiplicarIndu(arr1, arr2);
 
+        System.out.println("\nResultado");
+        imprimirArreglo(resultado);
     }
 
     private static int[] multiplicarIndu(int[] arr1, int[] arr2) {
+        int[] resultado = new int[arr1.length + arr2.length];
+        int[] acarreo = new int[resultado.length];
 
-        int[] resultado = new int[arr1.length+ arr2.length];
-        int[] acarreo = new int[arr1.length + arr2.length];
-        int i = arr2.length -1;
-        int j = arr1.length - 1;
+        multiplicarRecursivo(arr1, arr2, resultado, acarreo, arr2.length - 1, resultado.length - 1);
 
-        multiplicarRecursivo(arr1, arr2, resultado, acarreo, i, j);
-        imprimirResultado(resultado);
+        ajustarAcarreo(resultado, acarreo);
+
         return resultado;
     }
 
-    private static int[] multiplicarRecursivo(int[] arr1, int[] arr2, int[] resultado,
-                                              int[] acarreo, int i, int j) {
-        if (i == arr2.length) {
-            return resultado;
-        }
+    private static void multiplicarRecursivo(int[] arr1, int[] arr2, int[] resultado, int[] acarreo, int i, int k) {
+        if (i >= 0) {
+            k = resultado.length - (arr2.length - i);
 
-        int k = resultado.length - (arr2.length - i);
+            for (int j = arr1.length - 1; j >= 0; j--) {
+                resultado[k] += arr1[j] * arr2[i];
 
-        if (j >= 0) {
-            resultado[k] += arr1[j] * arr2[i];
+                if (resultado[k] >= 10) {
+                    acarreo[k - 1] += resultado[k] / 10;
+                    resultado[k] = resultado[k] % 10;
+                } else {
+                    acarreo[k - 1] = 0;
+                }
 
-            if (resultado[k] >= 10) {
-                acarreo[k - 1] += resultado[k] / 10;
-                resultado[k] = resultado[k] % 10;
-            } else {
-                acarreo[k - 1] = 0;
+                k--;
             }
 
-            return multiplicarRecursivo(arr1, arr2, resultado, acarreo, i, j - 1);
+            multiplicarRecursivo(arr1, arr2, resultado, acarreo, i - 1, k);
         }
-
-        if (k > 0) {
-            resultado[k] += acarreo[k - 1];
-
-            if (resultado[k] >= 10) {
-                acarreo[k - 1] += resultado[k] / 10;
-                resultado[k] = resultado[k] % 10;
-            } else {
-                acarreo[k - 1] = 0;
-            }
-
-            return multiplicarRecursivo(arr1, arr2, resultado, acarreo, i + 1, arr2.length - 1);
-        }
-
-        return multiplicarRecursivo(arr1, arr2, resultado, acarreo, i + 1, arr2.length - 1);
     }
 
-    private static void imprimirResultado(int[] resultado) {
-        System.out.println("Resultado");
-        for (int i = 0; i< resultado.length; i++){
-            System.out.print(resultado[i] + " ");
+    private static void ajustarAcarreo(int[] resultado, int[] acarreo) {
+        for (int i = resultado.length - 1; i >= 0; i--) {
+            resultado[i] += acarreo[i];
+
+            if (resultado[i] >= 10) {
+                acarreo[i - 1] += resultado[i] / 10;
+                resultado[i] = resultado[i] % 10;
+            }
+        }
+    }
+
+    private static void imprimirArreglo(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
         }
     }
 }
