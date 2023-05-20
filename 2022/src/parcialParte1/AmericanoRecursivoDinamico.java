8f1678d59dbed3fd15a9d17d12dda80eda78ebb5
AlissonCamposMar <alisson.camposm@uqvirtual.edu.co>
2023-05-20 18:18:20 -0500

    AmericanoRecursivoDinamico No funciona

--- 2022/src/parcialParte1/AmericanoRecursivoDinamico.java
+++ 2022/src/parcialParte1/AmericanoRecursivoDinamico.java
@@ -3,14 +3,23 @@ package parcialParte1;
 import java.math.BigInteger;
 import java.util.ArrayList;
 
+/**
+ * Aquí se encuentran los métodos de arreglos, recursivo y de ArrayList
+ */
 public class AmericanoRecursivoDinamico {
 
-    public static void main(String[] args){
-        BigInteger[] arr1 = {BigInteger.valueOf(9), BigInteger.valueOf(9), BigInteger.valueOf(9), BigInteger.valueOf(9), BigInteger.valueOf(9), BigInteger.valueOf(9), BigInteger.valueOf(9)};
-        BigInteger[] arr2 = {BigInteger.valueOf(9), BigInteger.valueOf(9), BigInteger.valueOf(9), BigInteger.valueOf(9), BigInteger.valueOf(9), BigInteger.valueOf(9), BigInteger.valueOf(9)};
+    public static void main(String[] args) {
+
+        BigInteger[] arr1 = {BigInteger.valueOf(9), BigInteger.valueOf(9),
+                BigInteger.valueOf(9), BigInteger.valueOf(9), BigInteger.valueOf(9),
+                BigInteger.valueOf(9), BigInteger.valueOf(9)};
+
+        BigInteger[] arr2 = {BigInteger.valueOf(9), BigInteger.valueOf(9),
+                BigInteger.valueOf(9), BigInteger.valueOf(9), BigInteger.valueOf(9),
+                BigInteger.valueOf(9), BigInteger.valueOf(9)};
 
         System.out.println("Arreglo multiplicando");
-        for(BigInteger l: arr1)
+        for (BigInteger l : arr1)
             System.out.print(l + " ");
 
         System.out.println("\nArreglo multiplicador");
@@ -19,55 +28,10 @@ public class AmericanoRecursivoDinamico {
         System.out.println();
 
         ArrayList<BigInteger> resultado = new ArrayList<>();
-        multiplicarAmericanoArrayList(arr1, arr2, resultado);
-    }
-
-    public static ArrayList<BigInteger> multiplicarAmericanoArrayList(BigInteger[] arr1, BigInteger[] arr2, ArrayList<BigInteger> resultado) {
-
-        BigInteger k = BigInteger.ZERO;
-        BigInteger acarreo = BigInteger.ZERO;
-        int longitud = arr1.length + arr2.length;
-
-        for(int i = 0; i< longitud; i++){
-            resultado.add(BigInteger.ZERO);
-        }
-
-        System.out.print("\n");
-
-        if(arr1.length > arr2.length){
-            BigInteger[] arrAux1 = arr1;
-            arr1 = arr2;
-            arr2 = arrAux1;
-        }
-
-        multiplicarRecursivo(arr1, arr2, resultado, arr2.length - 1, k, acarreo);
 
         imprimirResultadoArrayList(resultado);
-        return resultado;
     }
 
-    public static void multiplicarRecursivo(BigInteger[] arr1, BigInteger[] arr2,
-                                            ArrayList<BigInteger> resultado, int i, BigInteger k, BigInteger acarreo) {
-        if (i >= 0) {
-            k = BigInteger.valueOf(resultado.size() - (arr2.length - i));
-
-            multiplicarRecursivo(arr1, arr2, resultado, i - 1, k, acarreo);
-
-            for (int j = arr1.length - 1; j >= 0; j--) {
-                resultado.set(k.intValue(), arr1[j].multiply(arr2[i]).add(acarreo).add(resultado.get(k.intValue())));
-                if (resultado.get(k.intValue()).compareTo(BigInteger.TEN) >= 0) {
-                    acarreo = resultado.get(k.intValue()).divide(BigInteger.TEN);
-                    resultado.set(k.intValue(), resultado.get(k.intValue()).mod(BigInteger.TEN));
-                } else {
-                    acarreo = BigInteger.ZERO;
-                }
-                k = k.subtract(BigInteger.ONE);
-            }
-            resultado.set(k.intValue(), acarreo);
-            acarreo = BigInteger.ZERO;
-        }
-
-    }
 
 
     private static void imprimirResultadoArrayList(ArrayList<BigInteger> resultado) {
@@ -78,4 +42,8 @@ public class AmericanoRecursivoDinamico {
         System.out.println();
     }
 
+
+
 }
+
+
