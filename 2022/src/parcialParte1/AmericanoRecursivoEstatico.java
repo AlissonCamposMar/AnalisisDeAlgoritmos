20d5f2df58974b7fa0f9ccb33951500dcc738b7c
AlissonCamposMar <alisson.camposm@uqvirtual.edu.co>
2023-05-21 10:52:26 -0500

    AmericanoRecursivoEstadico funcionando bien

--- 2022/src/parcialParte1/AmericanoRecursivoEstatico.java
+++ 2022/src/parcialParte1/AmericanoRecursivoEstatico.java
@@ -1,42 +1,55 @@
 package parcialParte1;
 
+import java.math.BigInteger;
+
 public class AmericanoRecursivoEstatico {
+
     public static void main(String[] args){
-        int [] arr1 = {9,9,9,9,9,9,9};
-        int [] arr2 = {9,9,9,9,9,9};
+        BigInteger[] arr1 = {BigInteger.valueOf(9),
+                BigInteger.valueOf(9), BigInteger.valueOf(9),
+                BigInteger.valueOf(9), BigInteger.valueOf(9),
+                BigInteger.valueOf(9), BigInteger.valueOf(9)};
+
+        BigInteger[] arr2 = {BigInteger.valueOf(9),
+                BigInteger.valueOf(9), BigInteger.valueOf(9),
+                BigInteger.valueOf(9), BigInteger.valueOf(9),
+                BigInteger.valueOf(9), BigInteger.valueOf(9)};
 
 
         System.out.println("Arreglo multiplicando");
-        for(int l: arr1)
+        for(BigInteger l: arr1)
             System.out.print(l + " ");
 
         System.out.println("\nArreglo multiplicador");
-        for (int h : arr2)
+        for (BigInteger h : arr2)
             System.out.print(h + " ");
         System.out.println();
 
         multiplicarArreglosAmericanoRecursivo(arr1, arr2);
-        //multiplicarAmericanoArrayList(arr1, arr2);
 
     }
 
     //Este método evita que en la ejecución se desborde
     //Si el valor que hay en el arreglo 1 es mayor al valor del arreglo 2 entonces se intercambian
     //Es utilizado en el método recursivo
-    private static int[] multiplicarArreglosAmericanoRecursivo(int[] arr1, int[] arr2) {
-        int []resultado = new int[arr1.length+ arr2.length];
+    private static BigInteger[] multiplicarArreglosAmericanoRecursivo(BigInteger[] arr1, BigInteger[] arr2) {
 
-        if(arr1.length > arr2.length){
-            int [] arrAux1 = arr1;
+        BigInteger[] resultado = new BigInteger[arr1.length + arr2.length];
+
+        if (arr1.length > arr2.length) {
+            BigInteger[] arrAux1 = arr1;
             arr1 = arr2;
             arr2 = arrAux1;
         }
 
-        int acarreo = 0;
-        int i = arr2.length-1;
-        int j = arr1.length-1;
-        int k = resultado.length-1;
+        BigInteger acarreo = BigInteger.ZERO;
+        int i = arr2.length - 1;
+        int j = arr1.length - 1;
+        int k = resultado.length - 1;
 
+        for (int index = 0; index < resultado.length; index++) {
+            resultado[index] = BigInteger.ZERO;
+        }
 
         multiplicacionAmericanoRecursivo(arr1, arr2, resultado, acarreo, i, j, k);
         imprimirResultado(resultado);
@@ -44,19 +57,20 @@ public class AmericanoRecursivoEstatico {
 
     }
 
-    public static  int[] multiplicacionAmericanoRecursivo(int[] arr1, int[] arr2, int[] resultado,
-                                                          int acarreo, int i, int j, int k){
+    public static BigInteger[] multiplicacionAmericanoRecursivo(BigInteger[] arr1, BigInteger[] arr2, BigInteger[] resultado,
+                                                                BigInteger acarreo, int i, int j, int k){
 
         if(i==0 && j==0) {
-            resultado[k] += arr1[j] * arr2[i] + acarreo;
-            if (resultado[k] >= 10) {
-                acarreo = resultado[k] / 10;
-                resultado[k] = resultado[k] % 10;
+            resultado[k] = resultado[k].add(arr1[j].multiply(arr2[i])).add(acarreo);
+
+            if (resultado[k].compareTo(BigInteger.TEN) >= 0) {
+                acarreo = resultado[k].divide(BigInteger.TEN);
+                resultado[k] = resultado[k].mod(BigInteger.TEN);
             } else {
-                acarreo = 0;
+                acarreo = BigInteger.ZERO;
             }
             k--;
-            resultado[k]=acarreo;
+            resultado[k] = acarreo;
             //System.out.println(arr1[j] + "," + arr2[i]);
             return resultado;
 
@@ -64,31 +78,33 @@ public class AmericanoRecursivoEstatico {
             //System.out.println(arr1[j] + "," + arr2[i]);
 
             //Está en la posición j=0 e i= cualquier valor del arr1 (For anidado)
-            resultado[k] += arr1[j] * arr2[i] + acarreo;
-            if(resultado[k]>=10){
-                acarreo = resultado[k]/10;
-                resultado[k] = resultado[k]%10;
+            resultado[k] = resultado[k].add(arr1[j].multiply(arr2[i])).add(acarreo);
+
+            if(resultado[k].compareTo(BigInteger.TEN) >= 0){
+                acarreo = resultado[k].divide(BigInteger.TEN);
+                resultado[k] = resultado[k].mod(BigInteger.TEN);
             } else {
-                acarreo = 0;
+                acarreo = BigInteger.ZERO;
             }
             k--;
 
             //Termina el segundo for e itera el arr2 (primer for)
             resultado[k] = acarreo;
-            acarreo = 0;
+            acarreo = BigInteger.ZERO;
             i--;
-            j = arr1.length-1;
+            j = arr1.length - 1;
             k = resultado.length - (arr2.length - i);
             multiplicacionAmericanoRecursivo(arr1,arr2,resultado,acarreo,i,j,k);
         } else {
             //System.out.println(arr1[j] + "," + arr2[i]);
 
-            resultado[k] += arr1[j] * arr2[i] + acarreo;
-            if(resultado[k]>=10){
-                acarreo = resultado[k]/10;
-                resultado[k] = resultado[k]%10;
+            resultado[k] = resultado[k].add(arr1[j].multiply(arr2[i])).add(acarreo);
+
+            if(resultado[k].compareTo(BigInteger.TEN) >= 0){
+                acarreo = resultado[k].divide(BigInteger.TEN);
+                resultado[k] = resultado[k].mod(BigInteger.TEN);
             } else {
-                acarreo = 0;
+                acarreo = BigInteger.ZERO;
             }
             k--;
             j--;
@@ -99,7 +115,7 @@ public class AmericanoRecursivoEstatico {
         return resultado;
     }
 
-    private static void imprimirResultado(int[] resultado) {
+    private static void imprimirResultado(BigInteger[] resultado) {
         System.out.println("Resultado");
         for (int i = 0; i< resultado.length; i++){
             System.out.print(resultado[i] + " ");
