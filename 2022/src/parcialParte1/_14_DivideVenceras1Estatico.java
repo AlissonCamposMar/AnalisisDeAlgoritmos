1d6d5989bd4d2732da814f3d72a49daa4573ac1d
AlissonCamposMar <alisson.camposm@uqvirtual.edu.co>
2023-05-22 13:29:00 -0500

    divide y venceras 1 y 2 terminados

--- 2022/src/parcialParte1/_14_DivideVenceras1Estatico.java
+++ 2022/src/parcialParte1/_14_DivideVenceras1Estatico.java
@@ -1,4 +1,81 @@
 package parcialParte1;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigInteger;
+
 public class _14_DivideVenceras1Estatico {
+
+    public static void main(String[] args){
+        BigInteger arr1 = BigInteger.valueOf(9999999);
+        BigInteger arr2 = BigInteger.valueOf(9999999);
+
+
+
+        System.out.println();
+
+
+        System.out.println(dv1Multiplicacion(arr1, arr2));
+
+    }
+
+
+    // ---------------------------- Divide y vencer�s 1 (est�tico) -------------------------
+    public static BigInteger dv1Multiplicacion(BigInteger x, BigInteger y) {
+        // Verificar si los n�meros tienen un solo d�gito
+        if (x.compareTo(BigInteger.TEN) < 0 || y.compareTo(BigInteger.TEN) < 0) {
+            return x.multiply(y);
+        }
+
+        // Obtener el tama�o de los n�meros
+        int n = Math.max(x.bitLength(), y.bitLength());
+
+        // Dividir los n�meros en dos mitades
+        int m = n / 2;
+        BigInteger a = x.shiftRight(m);
+        BigInteger b = x.subtract(a.shiftLeft(m));
+        BigInteger c = y.shiftRight(m);
+        BigInteger d = y.subtract(c.shiftLeft(m));
+
+        // Calcular las tres multiplicaciones necesarias
+        BigInteger ac = dv1Multiplicacion(a, c);
+        BigInteger bd = dv1Multiplicacion(b, d);
+        BigInteger abcd = dv1Multiplicacion(a.add(b), c.add(d));
+
+        // Calcular el resultado final
+        return ac.shiftLeft(2 * m).add(abcd.subtract(ac).subtract(bd).shiftLeft(m)).add(bd);
+    }
+
+    public static BigInteger readFileToBigInteger(String filePath) {
+        BigInteger result = null;
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(new FileReader(filePath));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line = reader.readLine();
+
+            while (line != null) {
+                stringBuilder.append(line);
+                line = reader.readLine();
+            }
+
+            String fileContent = stringBuilder.toString();
+            result = new BigInteger(fileContent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return result;
+    }
+
 }
--- 2022/src/parcialParte1/_15_DivideVenceras2Estatico.java
+++ 2022/src/parcialParte1/_15_DivideVenceras2Estatico.java
@@ -1,4 +1,86 @@
 package parcialParte1;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigInteger;
+
 public class _15_DivideVenceras2Estatico {
+
+    public static void main(String[] args){
+        BigInteger arr1 = BigInteger.valueOf(9999999);
+        BigInteger arr2 = BigInteger.valueOf(9999999);
+
+
+
+        System.out.println();
+
+
+        System.out.println(dv2Multiplicacion(arr1, arr2));
+
+    }
+
+    // ---------------------------- Divide y vencer�s 2 (est�tico) -------------------------
+    public static BigInteger dv2Multiplicacion(BigInteger x, BigInteger y) {
+        int n = Math.max(x.bitLength(), y.bitLength());
+        if (n <= 2000) { // Caso base: n�meros peque�os
+            return x.multiply(y);
+        }
+        // Dividir x e y en dos partes iguales
+        BigInteger[] partesX = dividirEnPartes(x);
+        BigInteger[] partesY = dividirEnPartes(y);
+        BigInteger a = partesX[0], b = partesX[1];
+        BigInteger c = partesY[0], d = partesY[1];
+        // Calcular los cuatro productos parciales
+        BigInteger ac = dv2Multiplicacion(a, c);
+        BigInteger bd = dv2Multiplicacion(b, d);
+        BigInteger suma = dv2Multiplicacion(a.add(b), c.add(d));
+        BigInteger adbc = suma.subtract(ac).subtract(bd);
+        // Calcular el resultado final
+        BigInteger resultado = ac.multiply(BigInteger.TEN.pow(2 * n))
+                .add(adbc.multiply(BigInteger.TEN.pow(n)))
+                .add(bd);
+        return resultado;
+    }
+
+    // Divide un n�mero en dos partes iguales (redondeo hacia abajo)
+    private static BigInteger[] dividirEnPartes(BigInteger x) {
+        int n = x.bitLength() / 2;
+        BigInteger a = x.shiftRight(n);
+        BigInteger b = x.subtract(a.shiftLeft(n));
+        return new BigInteger[] {a, b};
+    }
+
+
+    public static BigInteger readFileToBigInteger(String filePath) {
+        BigInteger result = null;
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(new FileReader(filePath));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line = reader.readLine();
+
+            while (line != null) {
+                stringBuilder.append(line);
+                line = reader.readLine();
+            }
+
+            String fileContent = stringBuilder.toString();
+            result = new BigInteger(fileContent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return result;
+    }
+
 }
