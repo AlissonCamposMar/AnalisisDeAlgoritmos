3f3f80ddd9e223839614c7943490064d9f3a1a70
AlissonCamposMar <alisson.camposm@uqvirtual.edu.co>
2023-05-23 13:30:16 -0500

    cambio en el tipo de dato del método ruso

--- 2022/src/parcialParte1/_9_RusaIEstatica.java
+++ 2022/src/parcialParte1/_9_RusaIEstatica.java
@@ -1,18 +1,17 @@
 package parcialParte1;
 
 import java.math.BigInteger;
+import java.util.Arrays;
 
 public class _9_RusaIEstatica {
 
-    public static void main(String[] args){
+    /*public static void main(String[] args){
         BigInteger arr1 = BigInteger.valueOf(9999999);
         BigInteger arr2 = BigInteger.valueOf(9999999);
 
 
 
         System.out.println();
-
-
         System.out.println(rusoEstatico(arr1, arr2));
 
     }
@@ -29,6 +28,57 @@ public class _9_RusaIEstatica {
                 return rusoEstatico(n.multiply(dos), m.divide(dos)).add(n);
             }
         }
+    }*/
+
+    public static void main(String[] args) {
+        BigInteger[] arr1 = {BigInteger.valueOf(9999999)};
+        BigInteger[] arr2 = {BigInteger.valueOf(9999999)};
+
+        System.out.println();
+        System.out.println(Arrays.toString(rusoEstatico(arr1, arr2)));
     }
+
+    private static BigInteger dos = new BigInteger("2");
+
+    public static BigInteger[] rusoEstatico(BigInteger[] n, BigInteger[] m) {
+        if (m[0].equals(BigInteger.ZERO)) {
+            return new BigInteger[]{BigInteger.ZERO};
+        } else if (m[0].equals(BigInteger.ONE)) {
+            return n;
+        } else {
+            if (m[0].remainder(dos).equals(BigInteger.ZERO)) {
+                BigInteger[] result = rusoEstatico(doubleArray(n), divideArray(m));
+                return result;
+            } else {
+                BigInteger[] result = rusoEstatico(doubleArray(n), divideArray(m));
+                return addArrays(result, n);
+            }
+        }
+    }
+
+    private static BigInteger[] doubleArray(BigInteger[] arr) {
+        BigInteger[] doubled = new BigInteger[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            doubled[i] = arr[i].multiply(dos);
+        }
+        return doubled;
+    }
+
+    private static BigInteger[] divideArray(BigInteger[] arr) {
+        BigInteger[] divided = new BigInteger[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            divided[i] = arr[i].divide(dos);
+        }
+        return divided;
+    }
+
+    private static BigInteger[] addArrays(BigInteger[] arr1, BigInteger[] arr2) {
+        BigInteger[] sum = new BigInteger[arr1.length];
+        for (int i = 0; i < arr1.length; i++) {
+            sum[i] = arr1[i].add(arr2[i]);
+        }
+        return sum;
+    }
+
 
 }
