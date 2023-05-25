d0fcff2b78ba51d4bddbb4ef60ba18eddff8d43c
AlissonCamposMar <alisson.camposm@uqvirtual.edu.co>
2023-05-25 16:10:18 -0500

    cambio en el tipo de dato

--- 2022/src/parcialParte1/_14_DivideVenceras1Estatico.java
+++ 2022/src/parcialParte1/_14_DivideVenceras1Estatico.java
@@ -1,60 +1,250 @@
 package parcialParte1;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 public class _14_DivideVenceras1Estatico {
 
-    public static void main(String[] args){
-        BigInteger x = new BigInteger("9999999");
-        BigInteger y = new BigInteger("9999999");
+    //método de llamado dv1
+    public static int[] divideyVenceras1(int vec1[],int vec2[],int n) {
 
+        if(n==2) {//caso base
+            int[] resultado = new int[n+1];
+            resultado = AlgoritmoTradicional(vec1,vec2);
+            return resultado;
+        }
+        else {//crea 4 arreglosunidimensionales tamaño n/2
+            int[] x=new int[n/2];
+            int[] y=new int[n/2];
+            int[] z=new int[n/2];
+            int[] w=new int[n/2];
 
+            for(int i=0;i<n/2;i++) {
+                w[i]=vec1[i];//recibe la izquierda del multiplicador
+                y[i]=vec2[i];//recibe la izquierda del multiplicando
+                x[i]=vec1[i+(n/2)];//recibe la derecha del multiplicandorecibe la derecha del multiplicador
+                z[i]=vec2[i+(n/2)];
+            }
 
-        System.out.println();
+            //estos reciben los resultados parciales de la parte izquierda
+            int[] r=new int[2*n];
+            int[] auxr = new int[n];
+            iniceros(r,2*n);
+            auxr=divideyVenceras1(w,y,n/2);
 
+            for(int i=0;i<n;i++)
+            {
+                r[i]=auxr[i];
+            }
 
-        System.out.println(dv1Multiplicacion(x, y));
+            //estos reciben los resultados parciales de la parte derecha
+            int[] s = new int[n+(n/2)];
+            int[] auxs = new int[n+(n/2)];
+            iniceros(s,n+(n/2));
+            auxs=divideyVenceras1(w,z,n/2);
 
+            for(int i=0;i<n;i++)
+            {
+                s[i]=auxs[i];
+            }
+
+            int[] t=new int[(n/2)+n];
+            int[] auxt=new int[n];
+            iniceros(t,(n/2)+n);
+            auxt=divideyVenceras1(x,y,n/2);
+
+            for(int i=0;i<n;i++)
+            {
+                t[i]=auxt[i];
+            }
+
+            int[] u=new int[n];
+            iniceros(u,n);
+            u=divideyVenceras1(x,z,n/2);
+
+            int[] res=new int[2*n];
+            iniceros(res,2*n);
+            res=suma(r,2*n,s,n+(n/2));
+
+            //System.out.println("res1: "+Arrays.toString(res));
+            int[] res2=new int[(n/2)+n];
+            iniceros(res2,(n/2)+n);
+            res2=suma(t,n+(n/2),u,n);
+
+            //System.out.println("res2: "+Arrays.toString(res2));
+            int[] res3=new int[2*n];
+            iniceros(res3,2*n);
+
+            res3=suma(res,2*n,res2,(n/2)+n);
+            //System.out.println("res3: "+Arrays.toString(res3));
+            return res3;
+        }
+    }
+
+    //completa de ceros los arreglos hasta la potencia de 2 más cercana
+    static void iniceros(int arreglo[],int tamano)
+    {
+        for(int i=0; i<tamano; i++)
+        {
+            arreglo[i]=0;
+        }
+    }
+
+    //método para caso base
+    public static int[] AlgoritmoTradicional (int arreglo1[], int arreglo2[]) {
+        int resultado[]= new int [arreglo1.length+ arreglo2.length];
+        int k;
+        int acarreo = 0;
+
+        for (int i = arreglo2.length -1; i>=0; i--){
+
+            //Verifica a qué tan lejos está del borde derecho del arreglo resultado
+            k  = resultado.length - (arreglo2.length - i);
+            //Recorre el arreglo multiplicando desde la última posición
+            for (int j = arreglo1.length - 1; j >= 0; j--) {
+
+                //Realiza la multiplicación y suma sobre el resultado anterior
+                resultado[k] += arreglo1[j] * arreglo2[i] + acarreo ;
+                /**Condición que verifica si el resultado es igual o mayor a 10
+                 * Para indicar que se acumula en el acarreo y que queda almacenado en la posicion [k]
+                 * Ejem: 24/10 = 2--->acarreo(lo que se va a sumar en la siguiente multiplicación)
+                 * 24%10 = 4 ---> resultado[k]
+                 */
+                if (resultado[k] >= 10) {
+                    acarreo = resultado[k]/10;
+                    resultado[k] = resultado[k]%10;
+                } else {
+                    acarreo = 0;
+                }
+
+                k--;
+            }
+            resultado[k]=acarreo;
+            acarreo = 0;
+        }
+        return resultado;
+    }
+
+    //método para la suma de los arrays resultantes, controlando los acarreos
+    public static int[] suma(int[] vec1, int tam1, int[] vec2, int tam2) {
+        int[] res = new int[Math.max(tam1, tam2)];
+        int carry = 0;
+        int i = tam1 - 1;
+        int j = tam2 - 1;
+        int k = res.length - 1;
+
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (i >= 0) {
+                sum += vec1[i];
+                i--;
+            }
+            if (j >= 0) {
+                sum += vec2[j];
+                j--;
+            }
+            res[k] = sum % 10;
+            carry = sum / 10;
+            k--;
+        }
+
+        if (carry > 0) {
+            int[] newRes = new int[res.length + 1];
+            System.arraycopy(res, 0, newRes, 1, res.length);
+            newRes[0] = carry;
+            return newRes;
+        }
+        return res;
     }
 
 
-    // ---------------------------- Divide y vencer�s 1 (est�tico) -------------------------
-    public static BigInteger dv1Multiplicacion(BigInteger x, BigInteger y) {
+    public static void main(String[] args){
+        //2x2
+        BigInteger n0 = new BigInteger("93");
+        BigInteger num0 = new BigInteger("43");
 
-        // Verificar si alguno de los números es cero
-        if (x.equals(BigInteger.ZERO) || y.equals(BigInteger.ZERO)) {
-            return BigInteger.ZERO;
+        //4x4
+        BigInteger n1 = new BigInteger("9384");
+        BigInteger num1 = new BigInteger("4321");
+
+        //8x8
+        BigInteger n2 = new BigInteger("93849383");
+        BigInteger num2 = new BigInteger("43212456");
+
+        //16x16
+        BigInteger n3 = new BigInteger("9384938384738374");
+        BigInteger num3 = new BigInteger("4321245679876543");
+
+        //32x32
+        BigInteger n4 = new BigInteger("93849383847383748372838374837372");
+        BigInteger num4 = new BigInteger("43212456798765431234543224232344");
+
+        //64x64
+        BigInteger n5 = new BigInteger("9384938384738374837283837483737275950483832727273748484235577890");
+        BigInteger num5 = new BigInteger("4321245679876543123454322423234456788763222222222222222222221234");
+
+        //128x128
+        BigInteger n6 = new BigInteger("93849383847383748372838374837372759504838327272737484842355778900987654321245679876543123454322423234456789384938384738374837283");
+        BigInteger num6 = new BigInteger("43212456798765431234543224232344567887632222222222222222222212345678355778900987654321245679876543123454322423234456788763222222");
+
+        //256x256
+        BigInteger n7 = new BigInteger("9384938384738374837283837483737275950483832727273748484235577890098765432124567987654312345432242323445678938493838473837483728593849383847383748372838374837372759504838327272737484842355778900987654321245679876543123454322423234456789384938384738374837285");
+        BigInteger num7 = new BigInteger("4321245679876543123454322423234456788763222222222222222222221234567835577890098765432124567987654312345432242323445678876322222222222222222222123456789012357789009876543212456798765431234543224232344567887632222222222222222222212345678928603836596003857473");
+
+        List<BigInteger> lista1 = new ArrayList<>();
+        lista1.add(n0);
+        lista1.add(n1);
+        lista1.add(n2);
+        lista1.add(n3);
+        lista1.add(n4);
+        lista1.add(n5);
+        lista1.add(n6);
+        lista1.add(n7);
+
+        List<BigInteger> lista2 = new ArrayList<>();
+        lista2.add(num0);
+        lista2.add(num1);
+        lista2.add(num2);
+        lista2.add(num3);
+        lista2.add(num4);
+        lista2.add(num5);
+        lista2.add(num6);
+        lista2.add(num7);
+
+        for(int i=0; i<8; i++){
+            System.out.println("----------");
+            int[] arr1 = convertBigIntegerToArray(lista1.get(i));
+            int [] arr2 = convertBigIntegerToArray(lista2.get(i));
+            System.out.println(arr1.length+" X "+arr2.length);
+
+            double startTime = System.nanoTime();
+            BigInteger result = convertArrayToBigInteger(divideyVenceras1(arr1, arr2, arr1.length));
+            double endTime = System.nanoTime();
+            System.out.println("Tiempo: "+(endTime-startTime)/100_000);
+            System.out.println("Resultado: "+result);
+        }
+    }
+
+    public static int[] convertBigIntegerToArray(BigInteger number) {
+        String numberString = number.toString();
+        int[] array = new int[numberString.length()];
+
+        for (int i = 0; i < numberString.length(); i++) {
+            array[i] = Character.getNumericValue(numberString.charAt(i));
         }
 
-        // Verificar si los n�meros tienen un solo d�gito
-        if (x.compareTo(BigInteger.TEN) < 0 || y.compareTo(BigInteger.TEN) < 0) {
-            return x.multiply(y);
+        return array;
+    }
+
+    public static BigInteger convertArrayToBigInteger(int[] array) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < array.length; i++) {
+            stringBuilder.append(array[i]);
         }
 
-        // Obtener el tama�o de los n�meros
-        int n = Math.max(x.bitLength(), y.bitLength());
-
-        // Verificar si n es una potencia de dos
-        if (n % 2 != 0) {
-            n++;
-        }
-
-        // Dividir los n�meros en dos mitades
-        int m = n / 2;
-        BigInteger powerOfTwo = BigInteger.TWO.pow(m);
-
-        BigInteger a = x.divide(powerOfTwo);
-        BigInteger b = x.mod(powerOfTwo);
-        BigInteger c = y.divide(powerOfTwo);
-        BigInteger d = y.mod(powerOfTwo);
-
-        // Calcular las tres multiplicaciones necesarias
-        BigInteger ac = dv1Multiplicacion(a, c);
-        BigInteger bd = dv1Multiplicacion(b, d);
-        BigInteger adPlusBc = dv1Multiplicacion(a.add(b), c.add(d)).subtract(ac).subtract(bd);
-
-        // Combinar los resultados parciales para obtener el resultado final
-        return ac.multiply(powerOfTwo.pow(2)).add(adPlusBc.multiply(powerOfTwo)).add(bd);
+        return new BigInteger(stringBuilder.toString());
     }
 
 }
--- 2022/src/parcialParte1/_15_DivideVenceras2Estatico.java
+++ 2022/src/parcialParte1/_15_DivideVenceras2Estatico.java
@@ -4,48 +4,266 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 public class _15_DivideVenceras2Estatico {
+    private static final int IZQ = 0;
+    private static final int DER = 1;
+
+    //método para iniciar el algoritmo
+    public static int[] divideyVenceras2(int vec1[], int vec2[])
+    {
+
+        if ( vec1.length <= 100 || vec2.length <= 100 )
+        {
+            return multi( vec1, vec2 );
+        }
+        int tam = tamanio (vec1, vec2);
+        vec1 = completar (vec1, tam, IZQ);
+        vec2 = completar (vec2, tam, IZQ);
+        int w [] = splitArray ( vec1 , IZQ);
+        int x [] = splitArray ( vec1 , DER);
+        int y [] = splitArray ( vec2 , IZQ);
+        int z [] = splitArray ( vec2 , DER);
+        int m1 [] = divideyVenceras2(w, y);
+        int m3 [] = divideyVenceras2(x, z);
+        int r [] = divideyVenceras2(sumaArray( w, x ),sumaArray( y, z ));
+        int m2 [] = restaArray( r, sumaArray( m1, m3 ));
+        return sumaArray(sumaArray(completar(m1,m1.length+tam,DER),
+                completar(m2,m2.length + tam/2, DER )) , m3);
+    }
+
+    //método para completar el array
+    public static int[] completar(int vec[], int tam, int id)
+    {
+        int res[];
+        if (tam > vec.length)
+        {
+            int cant = tam - vec.length;
+            res = new int[tam];
+            if (id == IZQ)
+            {
+                for (int i = 0; i < res.length; i++)
+                {
+                    if (i<cant) res[i] = 0;
+                    else res[i] = vec [i - cant];
+                }
+                return res;
+            }
+            else
+            {
+                for (int i = 0; i < res.length; i++)
+                {
+                    if (i<vec.length) res[i] = vec [i];
+                    else res[i] = 0;
+                }
+                return res;
+            }
+        }
+        return vec;
+    }
+
+    //método para restar el array de ceros
+    public static int[] restaArray(int vec[], int vec2[])
+    {
+        int res[]=new int [vec.length];
+        int k,j=vec2.length-1;
+        int bvec = base ( vec ), bvec2 = base ( vec2 );
+        for(int i=vec.length-1; i>=bvec; i--)
+        {
+            if(j>=bvec2)
+            {
+                if( vec[i] >= vec2[j])
+                    res[i]= vec[i] - vec2[j];
+                else
+                {
+                    k=i;
+                    vec[k]+=10;
+                    while( vec[k-1]==0 && k>bvec)
+                    {
+                        vec[k-1]=9;
+                        k--;
+                    }
+                    vec[k-1]--;
+                    res[i]= vec[i] - vec2[j];
+                }
+                j--;
+            }
+            else
+                res[i]=vec[i];
+        }
+        return res;
+    }
+
+    //mpetodo para obtener la base2
+    public static int base(int v[]) {
+        int pos=0;
+        while (v [pos]==0 && pos < v.length -1)
+        {
+            pos++;
+        }
+        if(pos>2)
+            pos--;
+        return pos ;
+    }
+
+
+    //método para partir el array
+    public static int[] splitArray(int vec[], int id) {
+        int tam = vec.length;
+        int res[]=new int[tam/2];
+        if (id == IZQ)
+        {
+            for (int i = 0; i < tam/2; i++)
+                res[i] = vec [i];
+        }else
+        {
+            for (int i = tam/2; i <tam; i++)
+                res[i-tam/2] = vec [i];
+        }
+        return res;
+    }
+
+    //método para sumar los arrays resultantes
+    public static int[] sumaArray(int v1[], int v2[]) {
+        int res[];
+        int mayor[]=v1 ;
+        int menor[]= v2 ;
+        if (v2.length>v1.length)
+        {
+            mayor = v2;
+            menor = v1;
+        }
+        res = new int [mayor.length+1];
+        int j = menor.length - 1 ;
+        for(int i = res.length-1; i>0; i--)
+        {
+            if( j >= 0)
+            {
+                int temp= mayor[i-1]+menor[j];
+                res[i]+=temp;
+                res[i-1] =res[i]/10;
+                res[i]=res[i]%10;
+                j--;
+            }
+            else
+            {
+                res[i]+=mayor[i-1];
+                res[i-1] =res[i]/10;
+                res[i]=res[i]%10;
+            }
+        }
+        return res;
+    }
+
+    //método para definir el tamaño
+    private static int tamanio(int[] vec1, int[] vec2) {
+        int max = Math.max(vec1.length, vec2.length);
+        int tam = 1;
+        while (tam < max) {
+            tam <<= 1;
+        }
+        return tam;
+    }
+
+    //método para solucionar el caso base
+    private static int[] multi(int arreglo1[], int arreglo2[] ){
+        int resultado []= new int [arreglo1.length + arreglo2.length];
+        for ( int i = arreglo2.length-1 ; i >= 0; i-- ){
+            for ( int j = arreglo1.length-1; j >= 0 ; j-- ){
+                resultado [ i + j + 1 ] += arreglo1 [ j ] * arreglo2 [ i ];
+                resultado [ i + j ] += (resultado [ i + j + 1 ] / 10);
+                resultado [ i + j + 1 ] %= 10;
+            }
+        }
+        return resultado;
+    }
 
     public static void main(String[] args){
-        BigInteger arr1 = BigInteger.valueOf(9999999);
-        BigInteger arr2 = BigInteger.valueOf(9999999);
+        //2x2
+        BigInteger n0 = new BigInteger("93");
+        BigInteger num0 = new BigInteger("43");
 
+        //4x4
+        BigInteger n1 = new BigInteger("9384");
+        BigInteger num1 = new BigInteger("4321");
 
+        //8x8
+        BigInteger n2 = new BigInteger("93849383");
+        BigInteger num2 = new BigInteger("43212456");
 
-        System.out.println();
+        //16x16
+        BigInteger n3 = new BigInteger("9384938384738374");
+        BigInteger num3 = new BigInteger("4321245679876543");
 
+        //32x32
+        BigInteger n4 = new BigInteger("93849383847383748372838374837372");
+        BigInteger num4 = new BigInteger("43212456798765431234543224232344");
 
-        System.out.println(dv2Multiplicacion(arr1, arr2));
+        //64x64
+        BigInteger n5 = new BigInteger("9384938384738374837283837483737275950483832727273748484235577890");
+        BigInteger num5 = new BigInteger("4321245679876543123454322423234456788763222222222222222222221234");
 
+        //128x128
+        BigInteger n6 = new BigInteger("93849383847383748372838374837372759504838327272737484842355778900987654321245679876543123454322423234456789384938384738374837283");
+        BigInteger num6 = new BigInteger("43212456798765431234543224232344567887632222222222222222222212345678355778900987654321245679876543123454322423234456788763222222");
+
+        //256x256
+        BigInteger n7 = new BigInteger("9384938384738374837283837483737275950483832727273748484235577890098765432124567987654312345432242323445678938493838473837483728593849383847383748372838374837372759504838327272737484842355778900987654321245679876543123454322423234456789384938384738374837285");
+        BigInteger num7 = new BigInteger("4321245679876543123454322423234456788763222222222222222222221234567835577890098765432124567987654312345432242323445678876322222222222222222222123456789012357789009876543212456798765431234543224232344567887632222222222222222222212345678928603836596003857473");
+
+        List<BigInteger> lista1 = new ArrayList<>();
+        lista1.add(n0);
+        lista1.add(n1);
+        lista1.add(n2);
+        lista1.add(n3);
+        lista1.add(n4);
+        lista1.add(n5);
+        lista1.add(n6);
+        lista1.add(n7);
+
+        List<BigInteger> lista2 = new ArrayList<>();
+        lista2.add(num0);
+        lista2.add(num1);
+        lista2.add(num2);
+        lista2.add(num3);
+        lista2.add(num4);
+        lista2.add(num5);
+        lista2.add(num6);
+        lista2.add(num7);
+
+        for(int i=0; i<8; i++){
+            System.out.println("----------");
+            int[] arr1 = convertBigIntegerToArray(lista1.get(i));
+            int [] arr2 = convertBigIntegerToArray(lista2.get(i));
+            System.out.println(arr1.length+" X "+arr2.length);
+
+            double startTime = System.nanoTime();
+            BigInteger result = convertArrayToBigInteger(divideyVenceras2(arr1, arr2));
+            double endTime = System.nanoTime();
+            System.out.println("Tiempo: "+(endTime-startTime)/100_000);
+            System.out.println("Resultado: "+result);
+        }
     }
+    public static int[] convertBigIntegerToArray(BigInteger number) {
+        String numberString = number.toString();
+        int[] array = new int[numberString.length()];
 
-    // ---------------------------- Divide y vencer�s 2 (est�tico) -------------------------
-    public static BigInteger dv2Multiplicacion(BigInteger x, BigInteger y) {
-        int n = Math.max(x.bitLength(), y.bitLength());
-        if (n <= 2000) { // Caso base: n�meros peque�os
-            return x.multiply(y);
+        for (int i = 0; i < numberString.length(); i++) {
+            array[i] = Character.getNumericValue(numberString.charAt(i));
         }
 
-        // Igualar la cantidad de dígitos a la potencia de dos más cercana
-        n = (n / 2) + (n % 2);
-
-        // Dividir los números en mitades
-        BigInteger tenPowN = BigInteger.TEN.pow(n);
-        BigInteger a = x.divide(tenPowN);
-        BigInteger b = x.mod(tenPowN);
-        BigInteger c = y.divide(tenPowN);
-        BigInteger d = y.mod(tenPowN);
-
-        // Calcular las multiplicaciones intermedias
-        BigInteger ac = dv2Multiplicacion(a, c);
-        BigInteger bd = dv2Multiplicacion(b, d);
-        BigInteger adPlusBc = dv2Multiplicacion(a.add(b), c.add(d)).subtract(ac).subtract(bd);
-
-        // Calcular el resultado final
-        return ac.multiply(tenPowN.pow(2)).add(adPlusBc.multiply(tenPowN)).add(bd);
-
+        return array;
     }
 
+    public static BigInteger convertArrayToBigInteger(int[] array) {
+        StringBuilder stringBuilder = new StringBuilder();
 
+        for (int i = 0; i < array.length; i++) {
+            stringBuilder.append(array[i]);
+        }
+
+        return new BigInteger(stringBuilder.toString());
+    }
 }
